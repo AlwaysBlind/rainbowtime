8fb266042f93db0e7b1a8126d2b114f6295c2f0c
alwaysulf <ulfbaeltdjur@gmail.com>
2022-12-06 13:53:35 +0100

    refactor

--- src/disco.ts
+++ src/disco.ts
@@ -1,5 +1,8 @@
 import * as vscode from "vscode";
 
+const nColors = 15;
+const width = 3;
+
 const hslToHex = (h: number, s: number, l: number) => {
   l /= 100;
   const a = (s * Math.min(l, 1 - l)) / 100;
@@ -17,7 +20,6 @@ const getColor = (x: number, max: number) => {
   return hslToHex(Math.floor((x / max) * 360), 80, 80);
 };
 
-const nColors = 15;
 const colors = [...Array(nColors).keys()].map((x) => getColor(x, nColors));
 
 const decorations = colors.map((color) =>
@@ -25,8 +27,29 @@ const decorations = colors.map((color) =>
     color: color,
   })
 );
+
 const nDecos = decorations.length;
-const width = 3;
+
+const colorize = (editor: vscode.TextEditor, c: number) => {
+  const len = editor.document.getText().length;
+  decorations.forEach((deco, i) => {
+    const ranges = [];
+    for (
+      let idx = ((i + c) % nDecos) * width;
+      idx < len;
+      idx = idx + nDecos * width
+    ) {
+      ranges.push(
+        new vscode.Range(
+          editor.document.positionAt(idx),
+          editor.document.positionAt(idx + width)
+        )
+      );
+    }
+    editor.setDecorations(deco, []);
+    editor.setDecorations(deco, ranges);
+  });
+};
 
 export const disco = () => {
   const editor = vscode.window.activeTextEditor;
@@ -35,24 +58,7 @@ export const disco = () => {
   }
   let c = 0;
   setInterval(() => {
-    const len = editor.document.getText().length;
-    decorations.forEach((deco, i) => {
-      const ranges = [];
-      for (
-        let idx = ((i + c) % nDecos) * width;
-        idx < len;
-        idx = idx + nDecos * width
-      ) {
-        ranges.push(
-          new vscode.Range(
-            editor.document.positionAt(idx),
-            editor.document.positionAt(idx + width)
-          )
-        );
-      }
-      editor.setDecorations(deco, []);
-      editor.setDecorations(deco, ranges);
-    });
+    colorize(editor, c);
     c += 1;
   }, 300);
 };
