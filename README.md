008d3f41d3e7f2c6cfd35742aed090b30757471e
alwaysulf <ulfbaeltdjur@gmail.com>
2022-12-06 13:40:21 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#<span style="color:red"> R </span><span style="color:orange"> A </span><span style="color:yellow"> I </span><span style="color:green"> N </span><span style="color:blue"> B </span><span style="color:indigo"> O </span><span style="color:violet"> W </span>
---
+++ src/disco.ts
@@ -0,0 +1,58 @@
+import * as vscode from "vscode";
+
+const hslToHex = (h: number, s: number, l: number) => {
+  l /= 100;
+  const a = (s * Math.min(l, 1 - l)) / 100;
+  const f = (n: number) => {
+    const k = (n + h / 30) % 12;
+    const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+    return Math.round(255 * color)
+      .toString(16)
+      .padStart(2, "0");
+  };
+  return `#${f(0)}${f(8)}${f(4)}`;
+};
+
+const getColor = (x: number, max: number) => {
+  return hslToHex(Math.floor((x / max) * 360), 80, 80);
+};
+
+const nColors = 15;
+const colors = [...Array(nColors).keys()].map((x) => getColor(x, nColors));
+
+const decorations = colors.map((color) =>
+  vscode.window.createTextEditorDecorationType({
+    color: color,
+  })
+);
+const nDecos = decorations.length;
+const width = 3;
+
+export const disco = () => {
+  const editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    return;
+  }
+  let c = 0;
+  setInterval(() => {
+    const len = editor.document.getText().length;
+    decorations.forEach((deco, i) => {
+      const ranges = [];
+      for (
+        let idx = ((i + c) % nDecos) * width;
+        idx < len;
+        idx = idx + nDecos * width
+      ) {
+        ranges.push(
+          new vscode.Range(
+            editor.document.positionAt(idx),
+            editor.document.positionAt(idx + width)
+          )
+        );
+      }
+      editor.setDecorations(deco, []);
+      editor.setDecorations(deco, ranges);
+    });
+    c += 1;
+  }, 300);
+};
